a751918fd68db854cea9ecfe991927ad74b3a192
Sandeeep Kumar <webtrainer.in@gmail.com>
2019-01-20 08:27:30 +0530

    Implement CRUD Operation for Makes of Vehicle

--- vroom/Controllers/MakeController.cs
+++ vroom/Controllers/MakeController.cs
@@ -3,27 +3,85 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using vroom.AppDbContext;
 using vroom.Models;
 
 namespace vroom.Controllers
 {
     public class MakeController : Controller
     {
-        //make
-        //make/bikes
-        [Route("Make")]
-        [Route("Make/Bikes")]
-        public IActionResult Bikes()
+        private readonly VroomDbContext _db;
+
+        public MakeController(VroomDbContext db)
         {
-            Make make = new Make { Id = 1, Name = "Harley Davidson" };
-            return View(make);            
-           
+            _db = db;
         }
-        [Route("make/bikes/{year:int:length(4)}/{month:int:range(1,13)}")]
-        public IActionResult ByYearMonth(int year, int month)
+
+        public IActionResult Index()
         {
-            return Content(year + ";" + month);
+            return View(_db.Makes.ToList());
         }
+
+        //HTTP Get Method
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Make make)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Add(make);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(make);
+
+        }
+
+        //HTTP Get Method
+        public IActionResult Edit(int id)
+        {
+            var make = _db.Makes.Find(id);
+            if(make==null)
+            {
+                return NotFound();
+            }
+
+            return View(make);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, Make make)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Update(make);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(make);
+
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var make = _db.Makes.Find(id);
+            if (make == null)
+            {
+                return NotFound();
+            }
+            _db.Makes.Remove(make);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+
 
     }
 }
